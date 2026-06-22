751fb7132807ec79bc6d93b1421feb40f38a5662
Abhinav <abhinav.r1296@gmail.com>
2026-06-22 22:10:24 +0530

    MongoDB Atlas, Cluster, DataBase, models are covered in this

--- Lecture 4/src/app.js
+++ Lecture 4/src/app.js
@@ -1,6 +1,71 @@
-const express = require('express')
+const express = require('express');
+const noteModel = require('./models/note.model');
 
 const app = express()
+
+app.use(express.json())
+
+/* 
+API -
+POST
+GET
+PATCH
+DELETE
+*/
+
+app.post("/notes", async(req,res)=>{
+    
+    const data = req.body
+
+    await noteModel.create({
+        title : data.title,
+        description : data.description
+    })
+
+    res.status(201).json({
+        message: "Note created successfully"
+    })
+})
+
+app.get("/notes",async (req,res)=>{
+    
+    const notes = await noteModel.find() //gives array of objects or empty arr
+
+    // const notes = await noteModel.findOne({ //returns object or null
+    //     title: "test_title"
+    // })
+
+    res.status(200).json({
+        message: "Notes fetched Successfully",
+        notes : notes
+    })
+})
+
+app.delete('/notes/:id', async (req,res)=>{
+
+    const id = req.params.id
+
+    await noteModel.findOneAndDelete({
+        _id: id
+    })
+
+    res.status(200).json({
+        message: "Note Deleted",
+    })
+
+})
+
+app.patch('/notes/:id', async(req,res)=>{
+
+    const id = req.params.id
+    const description = req.body.description
+
+    await noteModel.findOneAndUpdate({_id: id},{description: description})
+
+    res.status(200).json({
+        message:"note updated"
+    })
+})
 
 module.exports = app
 
--- Lecture 4/src/db/db.js
+++ Lecture 4/src/db/db.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 async function connectDB() {
     try {
         await mongoose.connect(
-            "mongodb://abhinav:fPbLXMlLANSHVou8@ac-8shhg3w-shard-00-00.4nawyqj.mongodb.net:27017,ac-8shhg3w-shard-00-01.4nawyqj.mongodb.net:27017,ac-8shhg3w-shard-00-02.4nawyqj.mongodb.net:27017/?ssl=true&replicaSet=atlas-mfdcvb-shard-0&authSource=admin&appName=Cluster0"
+            "mongodb://abhinav:fPbLXMlLANSHVou8@ac-8shhg3w-shard-00-00.4nawyqj.mongodb.net:27017,ac-8shhg3w-shard-00-01.4nawyqj.mongodb.net:27017,ac-8shhg3w-shard-00-02.4nawyqj.mongodb.net:27017/halley?ssl=true&replicaSet=atlas-mfdcvb-shard-0&authSource=admin&appName=Cluster0"
             
         )
 
