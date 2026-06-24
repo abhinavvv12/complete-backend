5130805f1caf60a5d0fab2b1150dd0f33b032506
Abhinav <abhinav.r1296@gmail.com>
2026-06-24 16:55:24 +0530

    Project (Cloud service provider + Backend)

--- Lecture 5/src/app.js
+++ Lecture 5/src/app.js
@@ -29,4 +29,14 @@ app.post("/create-post", upload.single("image"), async (req, res) =>{
 
 })
 
+app.get("/posts", async (req,res) =>{
+
+    const posts = await postModel.find()
+
+    res.status(200).json({
+        message: "posts fetched Successfully",
+        posts
+    })
+})
+
 module.exports = app;
